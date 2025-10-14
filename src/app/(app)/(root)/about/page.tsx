5cc439194d43ba8dcb6624f9e837c4ea28cb3209
Kshitij <kshitij.suraj@gmail.com>
2025-10-14 19:02:53 +0530

    feat: add about page and user data
    - Created about page with user info
    - Added data file for about user

---
+++ src/app/(app)/(root)/about/page.tsx
@@ -0,0 +1,116 @@
+import { Separator } from "@/components/separator";
+import { Separator as Hr } from "@/components/ui/separator";
+import { ABOUT_USER } from "@/features/about/data/about";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <div className="mx-auto md:max-w-xl">
+      <Separator />
+      <div className=" px-4">
+        <h1 className="text-3xl font-semibold">This is Me</h1>
+        <Separator className="h-8" />
+        <Image
+          src="/images/profile.png"
+          alt="profile"
+          width={1200}
+          height={1200}
+          quality={100}
+          className="flex size-6 shrink-0 w-1/3 h-1/2 aspect-square object-cover filter grayscale"
+          priority
+        />
+        <Separator />
+        <div className="space-y-4">
+          <h2 className="text-2xl">{ABOUT_USER.whoami.title}</h2>
+          <p className="text-muted-foreground">{ABOUT_USER.whoami.firstPara}</p>
+          <p className="text-muted-foreground">
+            {ABOUT_USER.whoami.secondPara}
+          </p>
+        </div>
+        {/* <Separator /> */}
+        {/* <div className="space-y-4">
+          <h2 className="text-xl italic font-bold">Tools I Love</h2>
+          <div className="space-y-4">
+            {ABOUT_USER.tools.map((tool) => (
+              <div key={tool.category}>
+                <p className="text-muted-foreground text-lg font-semibold mb-2 border-b">
+                  {tool.category}
+                </p>
+                <div className="grid grid-cols-2 gap-4">
+                  {tool.list.map((tool) => (
+                    <Link
+                      href={tool.link}
+                      key={tool.name}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <p className="text-muted-foreground text-sm">
+                        {tool.subcategory &&
+                          tool.subcategory.map((category, index) =>
+                            index === tool.subcategory.length - 1
+                              ? `#${category}`
+                              : `#${category}, `
+                          )}
+                      </p>
+                      <span>{tool.name}</span>
+                    </Link>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div> */}
+        <Separator />
+        <Hr />
+        <Separator />
+        <div className="space-y-4">
+          <h2 className="text-xl italic font-bold">People I Follow</h2>
+          <div className="grid grid-cols-2 gap-8">
+            {ABOUT_USER.followedPeople.map((person) => (
+              <Link
+                href={person.link}
+                key={person.name}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <p className="text-muted-foreground text-sm">
+                  {person.category.map((category, index) =>
+                    index === person.category.length - 1
+                      ? `#${category}`
+                      : `#${category}, `
+                  )}
+                </p>
+                <span>{person.name}</span>
+              </Link>
+            ))}
+          </div>
+        </div>
+        <Separator />
+        <Hr />
+        <Separator />
+        <div className="space-y-4">
+          <h2 className="text-xl italic font-bold">Favorite List</h2>
+          <div>
+            {ABOUT_USER.favoriteList.map((fav) => (
+              <div
+                key={fav.category}
+                className="grid grid-cols-2 border-b last-of-type:border-0 py-4"
+              >
+                <p className="text-muted-foreground text-lg font-semibold mb-2 ">
+                  {fav.category}
+                </p>
+                <div className="space-y-2">
+                  {fav.list.map((tool) => (
+                    <p key={tool}>{tool}</p>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <Separator className="h-12" />
+    </div>
+  );
+}
---
+++ src/features/about/data/about.ts
@@ -0,0 +1,94 @@
+export const ABOUT_USER = {
+  whoami: {
+    title:
+      "I am a creative developer, passionate about tackling challenges through design solutions that are both meaningful and captivating.",
+    firstPara: `Hey there! I graduated in Computer Science and transitioned into a role as a Frontend Developer. I started with HTML/CSS and jumped  into  JavaScript, ReactJS, TailwindCSS, and NextJS.
+  Now, I'm diving into cool stuff like WebGL, Shaders, and ThreeJS for interactive web magic.`,
+    secondPara: `When I'm not coding, catch me having a blast with Blender, Figma, and Spline, playing  video games or sketching.
+  Also, I'm a bit of a space nerd - love reading about astrophysics and always up for deep space chats. Let's create awesome things together!`,
+  },
+  followedPeople: [
+    {
+      name: "Khyati Trehan",
+      link: "https://khyatitrehan.com",
+      category: ["Digital Art"],
+    },
+    {
+      name: "Sumit Hedge",
+      link: "https://dribbble.com/SumitH",
+      category: ["Design"],
+    },
+    {
+      name: "Codegrid",
+      link: "https://www.youtube.com/codegrid",
+      category: ["Frontend Web Development"],
+    },
+    {
+      name: "Grantt Abbitt",
+      link: "https://www.youtube.com/@grabbitt",
+      category: ["Digital Art", "3D Art"],
+    },
+  ],
+  tools: [
+    {
+      category: "Design",
+      list: [
+        {
+          name: "Figma",
+          link: "https://www.figma.com/",
+          subcategory: ["UI/UX & Icon Design"],
+        },
+        {
+          name: "Spline",
+          link: "https://spline.design/",
+          subcategory: ["3D Design"],
+        },
+        {
+          name: "Blender",
+          link: "https://www.blender.org/",
+          subcategory: ["3D Design"],
+        },
+      ],
+    },
+    {
+      category: "Development",
+      list: [
+        {
+          name: "Cursor",
+          link: "https://cursor.com/home",
+          subcategory: ["Text Editor"],
+        },
+      ],
+    },
+  ],
+  favoriteList: [
+    {
+      category: "Documentaries",
+      list: [
+        "The Minimalists: Less Is Now",
+        "Abstract: The Art of Design",
+        "Unknown: Cosmic Time Machine",
+        "Black Holes: The Edge of all we know",
+      ],
+    },
+    {
+      category: "Design Systems",
+      list: ["Apple", "Airbnb"],
+    },
+    {
+      category: "Books",
+      list: [
+        "The Subtle Art of Not Giving a F*ck",
+        "Deep Work",
+        "The Lean Startup",
+        "Ikigai",
+        "Tuesdays with Morrie",
+        "For one more day",
+      ],
+    },
+    {
+      category: "UI Inspiration",
+      list: ["Awwwards", "Behance", "Dribble"],
+    },
+  ],
+};
