1b5dff19ae7af08a55b69e17bf4823d18f99f5a7
Kshitij <kshitij.suraj@gmail.com>
2025-11-30 21:21:19 +0530

    feat(profile): add github/live links to projects
    - Added github and live links
    - Using tooltip component
    - Improved layout and spacing

--- src/features/profile/components/profile-body/projects/project-item.tsx
+++ src/features/profile/components/profile-body/projects/project-item.tsx
@@ -4,6 +4,7 @@ import { Icons } from "@/components/icons";
 import Link from "next/link";
 import { Post } from "@/features/blog/types/post";
 import { MonitorIcon } from "lucide-react";
+import { SimpleTooltip, Tooltip } from "@/components/ui/tooltip";
 
 export default function ProjectItem({
   classname,
@@ -13,10 +14,7 @@ export default function ProjectItem({
   project: Post;
 }) {
   return (
-    <Link
-      href={`/projects/${project.slug}`}
-      className={cn("w-full", classname)}
-    >
+    <div className={cn("w-full", classname)}>
       <div className="flex items-center py-4 gap-4 px-1">
         <div className="p-1 self-start min-w-fit bg-input rounded-lg border">
           {project.metadata.image ? (
@@ -26,7 +24,7 @@ export default function ProjectItem({
               width={32}
               height={32}
               quality={100}
-              className="flex size-6 shrink-0"
+              className="flex shrink-0"
               unoptimized
               aria-hidden="true"
             />
@@ -41,38 +39,56 @@ export default function ProjectItem({
         </div>
 
         <div>
-          <h3 className="text-sm font-medium">{project.metadata.title}</h3>
+          <div className="flex gap-4 items-center mb-2">
+            <Link
+              className="hover:underline"
+              href={`/projects/${project.slug}`}
+            >
+              <h3 className="text-sm font-medium">{project.metadata.title}</h3>
+            </Link>
+            <span className="text-xs text-muted-foreground">|</span>
+            <div className="flex gap-4 self-center">
+              {project.metadata.githubLink && (
+                <SimpleTooltip
+                  contentProps={{ sideOffset: 8, className: "p-1" }}
+                  content="Github"
+                >
+                  <Link
+                    href={project.metadata.githubLink}
+                    className="text-xs text-muted-foreground group flex items-center gap-1"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    onClick={(e) => e.stopPropagation()}
+                  >
+                    <Icons.github className="size-4" />
+                    {/* <span className="group-hover:underline">Github</span> */}
+                  </Link>
+                </SimpleTooltip>
+              )}
+              {project.metadata.liveLink && (
+                <SimpleTooltip
+                  contentProps={{ sideOffset: 8, className: "p-1" }}
+                  content="Live"
+                >
+                  <Link
+                    href={project.metadata.liveLink}
+                    className="text-xs text-muted-foreground group flex items-center gap-1"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    onClick={(e) => e.stopPropagation()}
+                  >
+                    <MonitorIcon className="size-4" />
+                    {/* <span className="group-hover:underline">Live</span> */}
+                  </Link>
+                </SimpleTooltip>
+              )}
+            </div>
+          </div>
           <p className="text-xs max-w-sm overflow-hidden truncate text-muted-foreground">
             {project.metadata.description}
           </p>
-          <div className="flex gap-4 mt-4">
-            {project.metadata.githubLink && (
-              <Link
-                href={project.metadata.githubLink}
-                className="text-xs text-muted-foreground group flex items-center gap-1"
-                target="_blank"
-                rel="noopener noreferrer"
-                onClick={(e) => e.stopPropagation()}
-              >
-                <Icons.github className="size-4" />
-                <span className="group-hover:underline">Github</span>
-              </Link>
-            )}
-            {project.metadata.liveLink && (
-              <Link
-                href={project.metadata.liveLink}
-                className="text-xs text-muted-foreground group flex items-center gap-1"
-                target="_blank"
-                rel="noopener noreferrer"
-                onClick={(e) => e.stopPropagation()}
-              >
-                <MonitorIcon className="size-4" />
-                <span className="group-hover:underline">Live</span>
-              </Link>
-            )}
-          </div>
         </div>
       </div>
-    </Link>
+    </div>
   );
 }
