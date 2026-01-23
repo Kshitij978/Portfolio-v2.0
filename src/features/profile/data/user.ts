c4b865e51983e665a9e03d1bf5fc102d5004163a
Kshitij <kshitij.suraj@gmail.com>
2026-01-23 21:50:21 +0530

    refactor(profile): SOCIAL_LINKS and switch to static icons

--- src/features/profile/data/user.ts
+++ src/features/profile/data/user.ts
@@ -1,3 +1,5 @@
+import { SOCIAL_LINKS } from "./social-links";
+
 export const USER = {
   firstName: "Kshitij",
   lastName: "Srivastava",
@@ -7,23 +9,7 @@ export const USER = {
   pronouns: "he/him",
   bio: "Building Immersive Web Experiences",
   flipSentences: ["Building Immersive Web Experiences", "Frontend Engineer"],
-  social: [
-    {
-      name: "Github",
-      link: "https://github.com/Kshitij978",
-      icon: "Github",
-    },
-    {
-      name: "Linkedin",
-      link: "https://www.linkedin.com/in/kssri/",
-      icon: "Linkedin",
-    },
-    {
-      name: "Codepen",
-      link: "https://codepen.io/Ks145",
-      icon: "Codepen",
-    },
-  ],
+  social: SOCIAL_LINKS,
   address: "Lucknow, India",
   email: "kshitij.suraj@gmail.com", // base64 encoded
   website: "https://kssri.com",
@@ -48,7 +34,7 @@ export const USER = {
   ],
   about: `
   I'm Kshitij Srivastava, a Frontend Engineer based in India.
-  
+
   With over 1.5 years of experience in the field, I take pride in delivering visually stunning and user-friendly designs that exceed client expectations.
 
 Whether it's collaborating with cross-functional teams or leading design projects, my attention to detail and dedication to excellence has earned me a reputation for delivering exceptional results.
