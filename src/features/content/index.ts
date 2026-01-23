876dff43f82777399419b3d6cd3679b4c303dcf8
Kshitij <kshitij.suraj@gmail.com>
2026-01-23 19:59:07 +0530

    feat(content): add barrel export

---
+++ src/features/content/index.ts
@@ -0,0 +1,18 @@
+export {
+  ContentCategory,
+  CATEGORY_CONFIG,
+  type ContentCategoryType,
+  type CategoryConfig,
+} from "./types";
+
+export {
+  getCategoryConfig,
+  getContentUrl,
+  isExternalContent,
+  isCategory,
+  getBlogPosts,
+  getProjectPosts,
+  getPostsByCategory,
+  getPostsByCategories,
+  sortByDateDesc,
+} from "./utils";
